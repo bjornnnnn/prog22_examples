361621805383892a263b416daee22f8791e80259
Björn Naeslund <bjorn.naeslund@r2m.se>
2023-09-26 12:51:07 +0200

    Initial commi,: utan gitignore-fil...

---
+++ encodeDecode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+string name = "Björn Næslund";
+Console.WriteLine("1. Plain text name is: {0} ",  name );
+
+Console.WriteLine("Now ENCODING string to Byte Array using codepage UTF8.");
+byte[] msg = System.Text.Encoding.UTF8.GetBytes(name);
+Console.WriteLine("2. Byte encoded name is: ");
+Console.WriteLine("{{{0}}}", string.Join(", ",  msg));
+
+
+Console.WriteLine("----------------------------------------------------------");
+
+
+Console.WriteLine("Now DECODING byte array to string using codepage UTF8.");
+string nameDecoded = System.Text.Encoding.UTF8.GetString(msg);
+Console.WriteLine("3. Encoded-Decoded string is:" + nameDecoded);
+
+
---
+++ websockets/Program.cs
@@ -0,0 +1,30 @@
+﻿
+class MyWebsocketClass
+{
+
+using HttpClientHandler handler = new()
+{
+    CookieContainer = cookies;
+UseCookies = cookies != null;
+ServerCertificateCustomValidationCallback = remoteCertificateValidationCallback;
+Credentials = useDefaultCredentials
+    ? CredentialCache.DefaultCredentials
+    : credentials;
+};
+if (proxy is null)
+{
+    handler.UseProxy = false;
+}
+else
+{
+    handler.Proxy = proxy;
+}
+if (clientCertificates?.Count > 0)
+{
+    handler.ClientCertificates.AddRange(clientCertificates);
+}
+HttpMessageInvoker invoker = new(handler);
+using ClientWebSocket cws = new();
+await cws.ConnectAsync(uri, invoker, cancellationToken);
+
+}
