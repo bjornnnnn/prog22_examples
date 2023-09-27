8ba67b8ff0b12e3a394eda6b174568c4b9789905
Björn Naeslund <bjorn.naeslund@r2m.se>
2023-09-27 13:51:50 +0200

    Hash

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ Exempelkod för labbar
 
 
 Dependency injection länk: https://learn.microsoft.com/en-us/aspnet/signalr/overview/advanced/dependency-injection
+
+
+Länk till Encrypting: https://learn.microsoft.com/en-us/dotnet/standard/security/encrypting-data
+
+Länk till Decrypting:https://learn.microsoft.com/en-us/dotnet/standard/security/decrypting-data
---
+++ hash/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+string messageString = @"lösenord-hemligt";
+Console.WriteLine(messageString);
+//Convert the string into an array of bytes.
+byte[] messageBytes = Encoding.UTF8.GetBytes(messageString);
+
+//Create the hash value from the array of bytes.
+byte[] hashValue = SHA256.HashData(messageBytes);
+
+//Display the hash value to the console.
+Console.WriteLine(Convert.ToHexString(hashValue));
