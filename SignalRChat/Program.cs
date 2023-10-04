7a5aaccce737e7fece8fd36b72862ea80a4052d0
david-nokto <david.nokto@gmail.com>
2023-10-04 13:53:06 +0200

    wip

--- SignalRChat/Program.cs
+++ SignalRChat/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.SignalR;
-using SignalRChat.Hubs;
 using SignalRChat.Controllers;
+using SignalRChat.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +15,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
